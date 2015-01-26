306c231156563771ad40892e843dfbb81bf7c19b
Darryl Dunkin <darryldunkin@zumiez.com>
2015-01-25 17:10:48 -0800

    duh

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+ln -s ~/dotfiles/.minttyrc ~/
+ln -s ~/dotfiles/.pryrc ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.vim ~/
 ln -s ~/dotfiles/.tmux.conf ~/
-ln -s ~/dotfiles/.minttyrc ~/
